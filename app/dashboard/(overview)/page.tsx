c7dd8de93c56c458de5d4500139f6da1bf309e44
Jacob Preciado <jacobpreciado@gmail.com>
2025-11-11 18:25:51 -0700

    Completed Tutorial

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,12 @@ import { lusitana } from '@/app/ui/fonts';
 import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton, LatestInvoicesSkeleton, CardsSkeleton } from '@/app/ui/skeletons';
- 
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Home',
+};
+
 export default async function Page() {
   return (
     <main>
